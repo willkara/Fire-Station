0abe995f08337f3b38dccafe5df779d0393ced5f
William Karavites <will@willkara.com>
2020-12-30 00:01:19 -0500

    Format amplify folder

--- amplify/backend/function/FireConstantsManager/src/app.js
+++ amplify/backend/function/FireConstantsManager/src/app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json())
 app.use(awsServerlessExpressMiddleware.eventContext())
 
 // Enable CORS for all methods
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Headers", "*")
   next()
@@ -35,60 +35,60 @@ app.use(function(req, res, next) {
  * Example get method *
  **********************/
 
-app.get('/item', function(req, res) {
+app.get('/item', function (req, res) {
   // Add your code here
   res.json({success: 'get call succeed!', url: req.url});
 });
 
-app.get('/item/*', function(req, res) {
+app.get('/item/*', function (req, res) {
   // Add your code here
   res.json({success: 'get call succeed!', url: req.url});
 });
 
 /****************************
-* Example post method *
-****************************/
+ * Example post method *
+ ****************************/
 
-app.post('/item', function(req, res) {
+app.post('/item', function (req, res) {
   // Add your code here
   res.json({success: 'post call succeed!', url: req.url, body: req.body})
 });
 
-app.post('/item/*', function(req, res) {
+app.post('/item/*', function (req, res) {
   // Add your code here
   res.json({success: 'post call succeed!', url: req.url, body: req.body})
 });
 
 /****************************
-* Example put method *
-****************************/
+ * Example put method *
+ ****************************/
 
-app.put('/item', function(req, res) {
+app.put('/item', function (req, res) {
   // Add your code here
   res.json({success: 'put call succeed!', url: req.url, body: req.body})
 });
 
-app.put('/item/*', function(req, res) {
+app.put('/item/*', function (req, res) {
   // Add your code here
   res.json({success: 'put call succeed!', url: req.url, body: req.body})
 });
 
 /****************************
-* Example delete method *
-****************************/
+ * Example delete method *
+ ****************************/
 
-app.delete('/item', function(req, res) {
+app.delete('/item', function (req, res) {
   // Add your code here
   res.json({success: 'delete call succeed!', url: req.url});
 });
 
-app.delete('/item/*', function(req, res) {
+app.delete('/item/*', function (req, res) {
   // Add your code here
   res.json({success: 'delete call succeed!', url: req.url});
 });
 
-app.listen(3000, function() {
-    console.log("App started")
+app.listen(3000, function () {
+  console.log("App started")
 });
 
 // Export the app object. When executing the application local this does nothing. However,
